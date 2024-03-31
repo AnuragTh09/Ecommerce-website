b7cea8dd1b82912254bb45d30b4628460c40e58c
AnuragTh09 <thakuranurag432@gmail.com>
2024-03-31 16:37:09 +0530

    update carousel component

--- Frontend/src/components/Carousel.jsx
+++ Frontend/src/components/Carousel.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const Carousel = () => {
   const [activeSlide, setActiveSlide] = useState(1);
@@ -12,22 +12,69 @@ const Carousel = () => {
   };
 
   return (
-    <div className="carousel w-full p-16 flex mt ">
-      <div id="slide1" className={`carousel-item relative w-full ${activeSlide === 1 ? 'block' : 'hidden'}`}>
-        <img src="https://daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.jpg" className="w-full" />
+    <div className="carousel w-full p-2 flex mt-20  flex-col items-center justify-center
+  ">
+
+      <div className="">
+      <div
+        id="slide1"
+        className={`carousel-item relative w-full ${
+          activeSlide === 1 ? "block" : "hidden"
+        }`}
+      >
+        <img
+          src="https://images.unsplash.com/photo-1531297484001-80022131f5a1?q=80&w=2020&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          className="w-full h-96 object-contain"
+        />
       </div>
-      <div id="slide2" className={`carousel-item relative w-full ${activeSlide === 2 ? 'block' : 'hidden'}`}>
-        <img src="https://daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.jpg" className="w-full" />
+      <div
+        id="slide2"
+        className={`carousel-item relative w-full ${
+          activeSlide === 2 ? "block" : "hidden"
+        }`}
+      >
+        <img
+          src="https://images.unsplash.com/photo-1434056886845-dac89ffe9b56?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          className="w-full h-96 object-contain"
+        />
       </div>
-      <div id="slide3" className={`carousel-item relative w-full ${activeSlide === 3 ? 'block' : 'hidden'}`}>
-        <img src="https://daisyui.com/images/stock/photo-1414694762283-acccc27bca85.jpg" className="w-full" />
+      <div
+        id="slide3"
+        className={`carousel-item relative w-full ${
+          activeSlide === 3 ? "block" : "hidden"
+        }`}
+      >
+        <img
+          src="https://images.unsplash.com/photo-1578242174372-e26b3681587f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          className="w-full h-96 object-contain"
+        />
       </div>
-      <div id="slide4" className={`carousel-item relative w-full ${activeSlide === 4 ? 'block' : 'hidden'}`}>
-        <img src="https://daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.jpg" className="w-full" />
+      <div
+        id="slide4"
+        className={`carousel-item relative w-full ${
+          activeSlide === 4 ? "block" : "hidden"
+        }`}
+      >
+        <img
+          src="https://images.unsplash.com/photo-1591970698020-e685959dcdaa?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          className="w-full h-96 object-contain"
+        />
       </div>
-      <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 bottom-1/2">
-        <button onClick={handlePrevSlide} className="btn btn-circle p-2 border border-black rounded-full w-10 hover:bg-gray-400">❮</button> 
-        <button onClick={handleNextSlide} className="btn btn-circle p-2 border border-black rounded-full w-10 hover:bg-gray-400">❯</button>
+      
+      </div>
+      <div className="flex items-center gap-40 mt-2">
+        <button
+          onClick={handlePrevSlide}
+          className="btn btn-circle p-2 border border-black rounded-full w-10 hover:bg-gray-400"
+        >
+          ❮
+        </button>
+        <button
+          onClick={handleNextSlide}
+          className="btn btn-circle p-2 border border-black rounded-full w-10 hover:bg-gray-400"
+        >
+          ❯
+        </button>
       </div>
     </div>
   );
