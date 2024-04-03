b4b07a3cf51dfaa414c117786e84c786cca2a353
AnuragTh09 <thakuranurag432@gmail.com>
2024-04-03 18:17:30 +0530

    updated db code and update db name

--- Backend/src/constant.js
+++ Backend/src/constant.js
@@ -1,1 +1,1 @@
-export const DB_NAME = 'FoodOrdering'
+export const DB_NAME = 'Ecommerce';
--- Backend/src/db/index.db.js
+++ Backend/src/db/index.db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import { DB_NAME } from '../constant.js'
 const connectDB = async() => {
     try{
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/ ${DB_NAME}`);
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
         console.log(`\n ☘️  MongoDb is connected successfully. Host at 🚀: ${connectionInstance.connection.host} `)
     }
     catch(error){
