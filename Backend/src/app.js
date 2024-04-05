dc0bf71b0641a004b6a39830fb5cce64b64a023a
AnuragTh09 <thakuranurag432@gmail.com>
2024-04-05 21:13:23 +0530

    added cors, cookie-parser

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,13 +1,20 @@
 import express from 'express';
-
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
 // importing Routes
 import userRoute from './routes/user.routes.js';
- 
+import productRoute from './routes/products.routes.js'
 
 const app = express();
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+}))
+
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-
+app.use(express.static("public"));
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
     res.send('API working with /api/v1')
@@ -15,12 +22,9 @@ app.get('/', (req, res) => {
 
 // using route
 app.use('/api/v1/user' , userRoute )
+app.use('/api/v1/product', productRoute)
 
+app.use('/uploads', express.static("uploads"))
 
-
-
-app.get('/test', (req, res) => {
-    res.send("Hello working correct");
-})
 
 export default app 
